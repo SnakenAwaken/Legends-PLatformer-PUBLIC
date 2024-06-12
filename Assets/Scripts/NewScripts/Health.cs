0f4af171e95f742cffcd8f5a676d51e711e74d68
SnakenAwaken <63108746+SnakenAwaken@users.noreply.github.com>
2024-06-12 15:36:29 +0300

    Init

---
+++ Assets/Scripts/NewScripts/Health.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float MaxHealth;
+    public float CurrentHealth;
+    public int Coins;
+
+    public float QDMG; // Queried Damage
+    void Start()
+    {
+        CurrentHealth = MaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(QDMG != 0)
+        {
+            CurrentHealth = CurrentHealth - QDMG;
+            QDMG = 0;
+        }
+    }
+}
